255808a60416abe23fa9f8f3fd5642a5e78d41a9
Sushan <sushanpoudel75@gmail.com>
2023-05-05 15:40:44 +0545

    Database connected

--- test.js
+++ test.js
@@ -1,8 +1,16 @@
 const express = require("express")
+const mongoose = require("mongoose")
 const httpServer = express()
 const routes = require("./routes")
 httpServer.use(express.json())
 httpServer.use(routes)
-httpServer.listen(3000, function() {
-    console.log("Server is listening on port 3000")
+mongoose.connect("mongodb://127.0.0.1:27017/databaseForPostsApp").then(function() {
+    console.log("Connected!!")
+
+    httpServer.listen(3000, function() {
+        console.log("Server is listening on port 3000")
+    })
+
+}).catch(function() {
+    console.log("Error connecting:" + e.toString())
 })
