4945f63f087348ba16151574c81b807824a9f4ec
Sushan <sushanpoudel75@gmail.com>
2023-05-05 13:02:59 +0545

    query used

--- routes.js
+++ routes.js
@@ -1,7 +1,12 @@
 const express = require("express")
 const router = express.Router()
-router.get("/", function(req, res) {
-    res.send("who are you? ")
+router.get("/hi", function(req, res) {
+    console.log(req.query)
+    res.send("hello," + req.query.name)
+
+})
+router.get("/bye", function(req, res) {
+    res.send("bye")
 
 })
 router.post("/", function(req, res) {
