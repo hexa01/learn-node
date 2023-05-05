0e0093a5092ae581e4d514f82b760ceb285b7499
Sushan <sushanpoudel75@gmail.com>
2023-05-05 12:10:35 +0545

    test.js server connected

--- test.js
+++ test.js
@@ -1,1 +1,9 @@
-console.log("hello from Sushan Poudel")
+const express = require("express")
+const httpServer = express()
+httpServer.listen(3000, function() {
+    console.log("Server is listening on port 3000")
+})
+httpServer.get("/", function(req, res) {
+    res.send("hello there")
+
+})
