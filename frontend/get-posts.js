94c7952525b49f2b1af8482b6874ee06076135ac
Sushan <sushanpoudel75@gmail.com>
2023-05-06 13:16:48 +0545

    insertOnePost function created

--- frontend/get-posts.js
+++ frontend/get-posts.js
@@ -1,7 +1,25 @@
 fetch("http://localhost:3000/")
     .then(function(res) {
-        console.log(res)
+        return res.text()
+    }).then(function(jres) {
+        console.log(jres);
+        const jsonData = JSON.parse(jres);
+        console.log(jsonData)
     })
     .catch(function(error) {
         console.log("Error fetching:" + error)
     })
+
+function insertOnePost() {
+    var toInsert = `<div class="post">
+        <div>
+            <div class="author">Sushan Poudel</div>
+            <div>on 29 May, 2023</div>
+        </div>
+        <div class="content">Hello there, this is my first post.</div>
+    </div>`;
+    const container = document.getElementById("posts-container")
+    container.insertAdjacentElement("beforeend", toInsert)
+}
+
+insertOnePost();
