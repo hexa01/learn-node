8d6a83ce28301fe1e5303a6744c8e11b06dc5cc9
Sushan <sushanpoudel75@gmail.com>
2023-05-06 12:18:20 +0545

    frontend start

--- routes.js
+++ routes.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const createNewPost = require("./functions");
 const router = express.Router();
-
-// getting JSON data from request body
+const Post = require("./db/posts.js")
+    // getting new post
 router.post("/", function(req, res) {
     createNewPost(req.body.postContent, req.body.postCreatedBy)
         .then(function(post) {
@@ -11,8 +11,15 @@ router.post("/", function(req, res) {
         .catch(function(error) {
             res.send("Error creating post")
         })
+})
 
-
+//getting all posts from database
+router.get("/", function(req, res) {
+    Post.find().then(function(posts) {
+        res.send(posts);
+    }).catch(function(error) {
+        res.send("Error getting posts:" + error);
+    })
 })
 
 
